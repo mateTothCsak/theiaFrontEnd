1242a53c864b45f82cd77b3967f998a6cca1f0e5
Mate Toth-Csak <tothcsakmate@gmail.com>
2019-03-21 20:41:22 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import {Route, Router, Redirect, Switch, BrowserRouter} from "react-router-dom";
+import {Route, Router, Switch} from "react-router-dom";
+import Index from './containers/Index/Index';
+import Game from './containers/Game/Game';
 import './App.css';
+import history from './History';
+
+
 
 class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            data : []
+        };
+    }
+
   render() {
-      return (
-      <Router >
+      return  (
+      <Router history={history}>
           <Switch>
-              <Route exact path="/" render={(props) => <Index {...props}/> } />
+              <Route exact path="/" render={() => <Index /> } />
+              <Route exact path="/game" render={() => <Game /> } />>
           </Switch>
       </Router>
       )
--- src/containers/Index/Index.js
+++ src/containers/Index/Index.js
@@ -1,3 +1,20 @@
 import React, { Component, Fragment } from 'react';
 
+class Index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
 
+    render() {
+        return (
+            <Fragment>
+                <p>Welcome to Theia Online!</p>
+                <p>This is the Main Page</p>
+            </Fragment>
+        )
+    }
+
+}
+
+export default Index;
